f6cfa078a5d2a31bf7e12db0d47d8abf8b9e0937
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-03-24 17:14:43 +0530

    Refactor code

--- app/src/main/java/com/destructo/mars/app/ui/roverList/RoverClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.destructo.mars.app.ui.roverList
-
-import com.destructo.mars.app.data.model.Rover
-
-class RoverClickListener(val clickListener: (rover : Rover?) -> Unit) {
-    fun onClick(rover: Rover?) = clickListener(rover)
-}
--- app/src/main/java/com/destructo/mars/app/ui/roverList/RoverListAdapter.kt
+++ app/src/main/java/com/destructo/mars/app/ui/roverList/RoverListAdapter.kt
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.destructo.mars.app.R
 import com.destructo.mars.app.data.model.Rover
 
-class RoverListAdapter(val clickListener: (Rover) -> Unit): ListAdapter<Rover, RoverListAdapter.ViewHolder>(RoverDiffUtilCallback()) {
+class RoverListAdapter(private val clickListener: (Rover) -> Unit): ListAdapter<Rover, RoverListAdapter.ViewHolder>(RoverDiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
