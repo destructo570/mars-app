f3e80a8b4c91a9cba5d13ceef91c7762bf904597
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-04-12 22:33:36 +0530

    Enable timber for logging

--- app/src/main/java/com/destructo/mars/app/MarsApplication.kt
+++ app/src/main/java/com/destructo/mars/app/MarsApplication.kt
@@ -2,7 +2,13 @@ package com.destructo.mars.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MarsApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
 }
